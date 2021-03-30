f42e43c96cc6ade0e17221d4c083e605b7839edb
Samuel Wessén <samuel.wessen@learnet.se>
2021-03-30 14:05:02 +0200

    Add project files.

---
+++ KenKata/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KenKata.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            RedirectToAction("Index", "Shop");
+            return View();
+        }
+    }
+}
